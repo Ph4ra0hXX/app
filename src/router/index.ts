03c085994869e6c38871ffc140214c368ce56637
Ph4ra0hXX <ph4ra0hx@gmail.com>
2025-12-15 20:16:03 -0300

    teste

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from "vue-router";
 
 import HomeView from "@/views/HomeView.vue";
 import ProductView from "@/views/ProductView.vue";
@@ -18,7 +22,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
